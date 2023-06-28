bed731487c6824dffd9a3c43306fe74fba1f18b8
Kenneth-Blanton <knsylvest@gmail.com>
2023-06-28 08:49:07 -0500

    Registration Form and Google Auth to Firestore

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,12 +1,22 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import {
   GoogleAuthProvider,
   FacebookAuthProvider,
   signInWithPopup,
 } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../utils/firebase";
+import { auth, db } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import {
+  addDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  setDoc,
+  Timestamp,
+} from "firebase/firestore";
 
 export default function Join() {
   const navigate = useNavigate();
@@ -15,11 +25,22 @@ export default function Join() {
 
   // Google Log-In
   const googleProvider = new GoogleAuthProvider();
-  const GoogleLogin = async () => {
+  const GoogleLogin = async (e) => {
+    e.preventDefault();
     try {
       const result = await signInWithPopup(auth, googleProvider);
       console.log(result.user);
-      navigate("/dashboard");
+      await setDoc(doc(db, "Users", result.user.uid), {
+        bio: "",
+        community: "",
+        created: Timestamp.fromDate(new Date()),
+        email: result.user.email,
+        firstName: result.user.displayName.split(" ")[0],
+        lastName: result.user.displayName.split(" ")[1],
+        img: result.user.photoURL,
+        userType: "",
+      });
+      // navigate("/dashboard");
     } catch (error) {
       console.log(error);
     }
@@ -37,6 +58,62 @@ export default function Join() {
     }
   };
 
+  const [warning, setWarning] = useState("");
+  const [community, setCommunity] = useState();
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPass, setConfPass] = useState("");
+
+  // Registration function
+  const Register = async (e) => {
+    e.preventDefault();
+    try {
+      if (community === undefined) {
+        setWarning("Select your Community");
+      } else if (firstName === "") {
+        console.log(firstName);
+        setWarning("Enter First Name");
+      } else if (email === "") {
+        setWarning("Enter Email");
+      } else if (password === "") {
+        setWarning("Enter password");
+      } else if (confPass !== password) {
+        setWarning("Match your password");
+      } else {
+        const q = query(collection(db, "Users"), where("email", "==", email));
+        console.log(email);
+        // const q = doc(db, "Users", email);
+
+        const queryProfile = await getDocs(q);
+
+        if (queryProfile.docs[0].exists()) {
+          console.log(queryProfile.docs[0].data());
+          setWarning("User already exists! Please sign in");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setWarning(null);
+      AddUser();
+      navigate("/dashboard");
+    }
+  };
+
+  // function to add user to firestore with info from registration form
+  function AddUser() {
+    addDoc(collection(db, "Users"), {
+      bio: "",
+      community,
+      created: "",
+      email,
+      firstName,
+      img: "",
+      userType: "",
+      password,
+    });
+  }
+
   return (
     <>
       <section className="bg-user-home user-pages d-flex align-items-center">
@@ -50,6 +127,7 @@ export default function Join() {
                 </a>
               </div>
               <div className="login-page bg-white shadow-lg rounded p-4 mt-4 position-relative">
+                <button onClick={() => auth.signOut()}>Sign Out</button>
                 <div className="text-center">
                   <h5 className="mb-4 pb-2">Join a Community</h5>
                 </div>
@@ -59,18 +137,21 @@ export default function Join() {
                       <div className="mb-3">
                         <label
                           className="my-1 mr-2"
-                          for="inlineFormCustomSelectPref"
+                          htmlFor="inlineFormCustomSelectPref"
                         >
                           Pick Your Community
                         </label>
                         <select
+                          value={community}
+                          onChange={(e) => setCommunity(e.target.value)}
+                          required
                           className="my-1 mr-sm-2 form-control"
                           id="inlineFormCustomSelectPref"
                         >
                           <option selected>Select your Community</option>
-                          <option value="1">Westend</option>
-                          <option value="2">Legacy Park </option>
-                          <option value="3">Not Listed</option>
+                          <option value="Westend">Westend</option>
+                          <option value="Legacy Park">Legacy Park </option>
+                          <option value="Not Listed">Not Listed</option>
                         </select>
                       </div>
                       <div className="mb-3">
@@ -83,6 +164,8 @@ export default function Join() {
                           name="s"
                           required
                           placeholder="First Name :"
+                          value={firstName}
+                          onChange={(e) => setFirstName(e.target.value)}
                         />
                       </div>
                     </div>
@@ -97,6 +180,8 @@ export default function Join() {
                           name="email"
                           required
                           placeholder="Your Email :"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
                         />
                       </div>
                     </div>
@@ -110,6 +195,8 @@ export default function Join() {
                           className="form-control"
                           required
                           placeholder="Password :"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
                         />
                       </div>
                     </div>
@@ -124,6 +211,8 @@ export default function Join() {
                           className="form-control"
                           required
                           placeholder="Confirm Password :"
+                          value={confPass}
+                          onChange={(e) => setConfPass(e.target.value)}
                         />
                       </div>
                     </div>
@@ -148,8 +237,14 @@ export default function Join() {
                         </div>
                       </div>
                     </div>
+                    <div className="warning text-danger text-center my-2">
+                      {warning}
+                    </div>
                     <div className="col-12">
-                      <button className="btn btn-primary w-100">
+                      <button
+                        className="btn btn-primary w-100"
+                        onClick={Register}
+                      >
                         Register
                       </button>
                     </div>
